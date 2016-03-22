0df5155b4d30be64e8afd1310a127d9678df16a9
Mohamed Ismail <mihafez.mi@gmail.com>
2016-03-22 15:05:17 +0200

    list that we will work on with functions needed

---
+++ oslisttry/oslisttry/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "List.h"
+using namespace std;
+int main()
+{
+//	string process_name;
+
+	cout << "hello world" << endl;
+
+system("pause");
+return 0;
+}
